df295d3dcf7ab95b3e86ea28ae744da4bca39652
Aoranun <aiaoy1479@gmail.com>
2019-02-10 21:42:22 +0700

    input number #complete

--- input_num/views.py
+++ input_num/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def input(request):
+    input_num = ""
+    if request.POST:
+        input_num = request.POST['name']
+        try:
+            if float(input_num)*0 == 0:
+                pass
+        except ValueError:
+            input_num = "It's not number."
+        return render(request, 'input.html', {'out_num': input_num})
     return render(request, 'input.html')
