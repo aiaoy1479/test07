103f2f124294c8a6de237c55f843392c86c30283
Aoranun <aiaoy1479@gmail.com>
2019-02-10 20:23:31 +0700

    mulitiplication(2)

--- show_mul/views.py
+++ show_mul/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def show_num(request, num):
     mul = []
     for i in range(1,13,1):
-        mul.append(str(num*i))
+        mul.append(str(num) +" X " + str(i) + " = " + str(num*i))
     return render(request, 'mul.html', {'show_num':num, 'mul_list':mul})
