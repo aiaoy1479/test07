6240745f1b0c93ebdfeca782d6e2c4de61851796
Aoranun <aiaoy1479@gmail.com>
2019-02-14 14:00:32 +0700

    Total input #1

--- input_num/models.py
+++ input_num/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Number(models.Model):
+    num_input = models.CharField(max_length=20)
+
+class Total_input(models.Model):
+    num = models.ForeignKey(Number, on_delete=models.CASCADE)
+    total = models.IntegerField()
+
--- input_num/views.py
+++ input_num/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Number, Total_input
 
 def input(request):
     input_num = ""
     if request.POST:
-        input_num = request.POST['name']
+        num = request.POST['name']
         mul = []
+        number = Number()
+        number.num_input = num
+        number.save()
+        show_num = 
         try:
-            num = int(float(input_num))
+            num = int(float(num))
             for i in range(1,13,1):
                 mul.append(str(num) + " X " + str(i) + " = " + str(num*i))
         except ValueError:
             mul.append("It's not number.")
-        return render(request, 'input.html', {'mul_list':mul})
+        return render(request, 'input.html', {'mul_list':mul, 'show_num': show_num})
     return render(request, 'input.html')
