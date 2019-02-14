d9efeef7855aa940334cc6a0d180f5f97278ffe0
Aoranun <aiaoy1479@gmail.com>
2019-02-14 15:24:55 +0700

    Total input #2(show number)

--- input_num/models.py
+++ input_num/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class Number(models.Model):
-    num_input = models.CharField(max_length=20)
-
 class Total_input(models.Model):
-    num = models.ForeignKey(Number, on_delete=models.CASCADE)
+    num = models.CharField(max_length=20)
     total = models.IntegerField()
+
+    def __str__(self):
+        return self.num
 
--- input_num/views.py
+++ input_num/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Number, Total_input
+from .models import Total_input
 
 def input(request):
-    input_num = ""
+    num_input = ""
+    number = []
     if request.POST:
-        num = request.POST['name']
+        num_input = request.POST['name']
         mul = []
-        number = Number()
-        number.num_input = num
-        number.save()
-        show_num = 
+        total = Total_input(num = num_input, total = 1)
+        total.save()
+        show_total = Total_input.objects.all()
+
         try:
-            num = int(float(num))
+            number = int(float(num_input))
             for i in range(1,13,1):
-                mul.append(str(num) + " X " + str(i) + " = " + str(num*i))
+                mul.append(str(number) + " X " + str(i) + " = " + str(number*i))
         except ValueError:
             mul.append("It's not number.")
-        return render(request, 'input.html', {'mul_list':mul, 'show_num': show_num})
+        return render(request, 'input.html', {'mul_list':mul, 'total': show_total})
     return render(request, 'input.html')
